cb16a9fc3252f0a4258cbfc21fb03b4fd37fe977
Florian Merz <florian.merz@orcaya.com>
2024-08-30 01:26:12 +0200

    feat: improve reaction bg visibility

--- src/components/SlackReactionRenderer.styles.ts
+++ src/components/SlackReactionRenderer.styles.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 import { Color } from '../styles/color';
+import { transparentize } from 'polished';
 
 export const Container = styled.span`
   display: inline-flex;
   padding: 5px 10px;
   gap: 10px;
   border-radius: ${props => props.theme.borderRadius.medium};
-  background-color: ${Color.Neutral_800};
+  background-color: ${transparentize(0.3, Color.Neutral_700)};
 `;
 
 export const Count = styled.span`
